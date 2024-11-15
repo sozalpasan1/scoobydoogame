1b6c6c90fb842d1c93af51dc9b27ab0a04056ab2
sean <sozalpasan1@hwemail.com>
2024-11-15 13:31:04 -0800

    added playcanvaas

---
+++ public/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2114265.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 204107771, 204107773, 204107775, 204107769, 204107789 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/204107772/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/204107975/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/204107784/1/ammo.js', 'preload' : true},
+];
